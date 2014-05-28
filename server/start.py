0d6f02897e2f5e3e5ca2a2ae4912c8588e60f5e7
John Flanagan <johnflan@gmail.com>
2014-05-28 13:17:11 +0100

    Basic login functionality

--- server/start.py
+++ server/start.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
-from flask import Flask, request
+from flask import Flask, request, abort, url_for, redirect
 import json
 import MySQLdb
+import hashlib
 
 app = Flask(__name__)
 
@@ -13,5 +14,26 @@ def hello():
     return "Welcome to Alaughabet"
 
 
+@app.route("/login", methods = ['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+   
+    if (username == "johnflan"):
+        passKey = hashlib.sha224(password).hexdigest()
+        return redirect(url_for('home', username=username, key=passKey))
+    else:
+        abort(401)
+
+@app.route("/home", methods = ['GET'])
+def home():
+    
+    if(username != johnflan):
+        abort(401)
+    else:
+        return "home for user " + user + " key:" + key
+    
+    return "home";
+
 if __name__ == "__main__":
-        app.run()
+        app.run(host="0.0.0.0", port=5000)

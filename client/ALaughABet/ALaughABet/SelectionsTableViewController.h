1bdcac7e34384d1f05e632ef9d7aa02a00ac132f
Lar Judge <lar@larjudge.com>
2014-05-28 13:38:09 +0100

    shell of iphone application@

---
+++ client/ALaughABet/ALaughABet/SelectionsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectionsTableViewController.h
+//  ALaughABet
+//
+//  Created by Lar Judge on 28/05/2014.
+//  Copyright (c) 2014 Lar Judge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectionsTableViewController : UITableViewController
+
+@end
---
+++ client/ALaughABet/ALaughABetTests/ALaughABetTests.m
@@ -0,0 +1,34 @@
+//
+//  ALaughABetTests.m
+//  ALaughABetTests
+//
+//  Created by Lar Judge on 28/05/2014.
+//  Copyright (c) 2014 Lar Judge. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ALaughABetTests : XCTestCase
+
+@end
+
+@implementation ALaughABetTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
